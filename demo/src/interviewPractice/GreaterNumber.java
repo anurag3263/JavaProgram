3c2fb279df86e84cc942d1e8bd4e61ae468d41e9
anurag3263 <80739367+anurag3263@users.noreply.github.com>
2022-04-05 15:00:33 +0530

    Add files via upload

---
+++ demo/src/interviewPractice/GreaterNumber.java
@@ -0,0 +1,16 @@
+package Start1;
+
+public class GreaterNumber {
+
+	public static void main(String[] args) {
+		// Find greater number without using If and Else
+		
+		int a=8;
+		int b=4;
+		int c=a>b?a:b;
+		
+		System.out.println("Greater  number amounst given two number is :  "+c);
+
+	}
+
+}
