20f77675d42f00ff1d1f51063e20bfe5cbea6715
anuragchaturvedi <anuragchaturvedi@localhost>
2023-07-12 01:21:48 +0530

    calendar date logic

--- demo/src/sSDET13/GetDateAndTime.java
+++ demo/src/sSDET13/GetDateAndTime.java
@@ -1,6 +1,7 @@
 package sSDET13;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class GetDateAndTime {
@@ -19,7 +20,28 @@ public class GetDateAndTime {
 		System.out.println(month_year);
 		System.out.println(month_year.substring(0, 3));
 		System.out.println(day);
-		System.out.println(dayMonth);
+		System.out.println("Current Date : "+dayMonth);
+		
+		
+		// Get Future Date
+		
+		Calendar calendar=Calendar.getInstance();
+		//c.setTime(new Date()); // Now use today date.
+		
+		calendar.setTime(d);
+		calendar.add(Calendar.DATE, 22);
+		System.out.println("Future Date with Current Time : "+calendar.getTime());
+		String futureDate= formatter.format(calendar.getTime());
+		String split[]=futureDate.split("-"); 
+		String monthName = split[1];
+		String Futureday = split[0]; 
+		
+		String monthLetter=monthName.substring(0, 3);
+		String dayMonthCombine=monthLetter+" "+Futureday;
+		System.out.println("Future date : "+dayMonthCombine);
+		
+		calendar.add(Calendar.MONTH, 7);
+		System.out.println(monthName);
 	}
 
 }
