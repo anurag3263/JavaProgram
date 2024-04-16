cb4dfcf6d3fba7957838d181d9459c2925b40a50
anuragchaturvedi <anuragchaturvedi@localhost>
2024-04-16 23:04:05 +0530

    map

---
+++ demo/src/practice2024/MapTraversing.java
@@ -0,0 +1,22 @@
+package practice2024;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class MapTraversing {
+
+	public static void main(String[] args) {
+		Map<String, String> map=new HashMap<>();
+		map.put("anurag", "10");
+		map.put("Rohit", "45");
+		
+		for(Entry<String, String> string :map.entrySet()) {
+			System.out.println("Key :"+string.getKey()+" = Value : "+string.getValue());
+		}
+//		        Key :Rohit = Value : 45
+//				Key :anurag = Value : 10
+	
+	}
+
+}
